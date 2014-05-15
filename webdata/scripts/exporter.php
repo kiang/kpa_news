87f4d902628730f91abc340197bca0b97dbe4b5e
kiang <kiange@gmail.com>
2014-05-15 23:04:37 +0800

    check resource before diving into database

--- webdata/scripts/exporter.php
+++ webdata/scripts/exporter.php
@@ -1,51 +1,58 @@
 #!/usr/bin/env php
 <?php
-if (!file_exists('news-list.csv.gz')) {
-    include(__DIR__ . '/../init.inc.php');
-    Pix_Table::$_save_memory = true;
+if (!file_exists('exporter.token')) {
+    file_put_contents('exporter.token', '1');
 
     $fp1 = gzopen('news-list.csv.gz', 'w');
     $fp2 = gzopen('news-content.csv.gz', 'w');
 
-    fputcsv($fp1, array(
-        'news_id',
-        'url',
-        'source',
-        'created_at',
-    ));
+    if (false !== $fp1 && false !== $fp2) {
+        include(__DIR__ . '/../init.inc.php');
+        Pix_Table::$_save_memory = true;
 
-    fputcsv($fp2, array(
-        'news_id',
-        'time',
-        'title',
-        'body',
-    ));
+        fputcsv($fp1, array(
+            'news_id',
+            'url',
+            'source',
+            'created_at',
+        ));
 
-    $start = 0;
-    while (true) {
-        $news_pool = array();
-        foreach (News::search(1)->order('id ASC')->after(array('id' => $start))->limit(100) as $news) {
-            $news_pool[] = $news->id;
-            error_log($news->id);
-            fputcsv($fp1, array(
-                $news->id,
-                $news->url,
-                $news->source,
-                $news->created_at,
-            ));
-        }
-        if (!count($news_pool)) {
-            break;
-        }
+        fputcsv($fp2, array(
+            'news_id',
+            'time',
+            'title',
+            'body',
+        ));
 
-        foreach (NewsInfo::search(1)->searchIn('news_id', $news_pool)->order(array('news_id', 'time')) as $info) {
-            fputcsv($fp2, array(
-                $info->news_id,
-                $info->time,
-                $info->title,
-                str_replace("\n", '\n', $info->body),
-            ));
+        $start = 0;
+        while (true) {
+            $news_pool = array();
+            foreach (News::search(1)->order('id ASC')->after(array('id' => $start))->limit(100) as $news) {
+                $news_pool[] = $news->id;
+                error_log($news->id);
+                fputcsv($fp1, array(
+                    $news->id,
+                    $news->url,
+                    $news->source,
+                    $news->created_at,
+                ));
+            }
+            if (!count($news_pool)) {
+                break;
+            }
+
+            foreach (NewsInfo::search(1)->searchIn('news_id', $news_pool)->order(array('news_id', 'time')) as $info) {
+                fputcsv($fp2, array(
+                    $info->news_id,
+                    $info->time,
+                    $info->title,
+                    str_replace("\n", '\n', $info->body),
+                ));
+            }
+            $start = $news->id;
         }
-        $start = $news->id;
+        gzclose($fp1);
+        gzclose($fp2);
     }
+    //unlink('exporter.token');
 }
